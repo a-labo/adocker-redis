20f8f3bbe660c689fafdf59370e0a2714dd85962
Taka Okunishi <okunishinishi@gmail.com>
2017-05-24 22:11:22 +0900

    Version incremented to 1.0.3

--- test/adocker_redis_test.js
+++ test/adocker_redis_test.js
@@ -5,7 +5,7 @@
 'use strict'
 
 const adockerRedis = require('../lib/adocker_redis.js')
-const assert = require('assert')
+const { equal } = require('assert')
 const co = require('co')
 
 describe('adocker-redis', function () {
@@ -22,9 +22,22 @@ describe('adocker-redis', function () {
   it('Adocker redis', () => co(function * () {
     let redis = adockerRedis('adocker-redis-test-01')
 
-    let { run, remove, logs } = redis.cli()
+    let { run, remove, logs, stop, isRunning, hasBuild } = redis.cli()
+
+    equal(yield isRunning(), false)
+    equal(yield hasBuild(), false)
     yield run()
+
+    equal(yield isRunning(), true)
+    equal(yield hasBuild(), true)
+
     yield logs()
+
+    yield stop()
+
+    equal(yield isRunning(), false)
+    equal(yield hasBuild(), true)
+
     yield remove({ force: true })
   }))
 })
